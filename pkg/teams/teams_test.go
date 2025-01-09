491325342ef7e3d247394d2792df72ee13345435
ben-harvey <benjaminriderharvey@gmail.com>
2025-01-09 08:59:38 -0500

    update tests and configure git for private module

--- pkg/teams/teams_test.go
+++ pkg/teams/teams_test.go
@@ -1,8 +1,11 @@
 package teams
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"io"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -14,27 +17,51 @@ var expectedAccountsToTeams = map[Account]string{
 	{ID: "account 22", Environment: "prod", RoleARN: "arn:aws:iam::000000000014:role/CustomRole"}: "Test Team 2",
 }
 
-func TestParseTeamMap(t *testing.T) {
-	// this test checks if the team map is parsed correctly for a valid team map
-	actualAccountsToTeams, err := ParseTeamMap("team_map_test_valid.json")
+// take a path to a test JSON file and encode it to a base64 string
+func base64EncodeTestJSON(filePath string) (string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
-		t.Errorf("ERROR: could not extract team map from test file: %s", err)
+		return "", fmt.Errorf("failed to valid JSON file: %s", err)
+	}
+	defer file.Close()
+	fileContents, err := io.ReadAll(file)
+	if err != nil {
+		return "", fmt.Errorf("failed to read file: %s", err)
+	}
+	return base64.StdEncoding.EncodeToString(fileContents), nil
+}
+
+// this test checks if the team map is parsed correctly for a valid team map. it first converts the valid JSON to the expected base64 encoded string
+func TestParseTeamMap(t *testing.T) {
+	validStr, err := base64EncodeTestJSON("team_map_test_valid.json")
+	if err != nil {
+		t.Errorf("failed to read valid JSON file: %s", err)
+	}
+	actualAccountsToTeams, err := ParseTeamMap(validStr)
+	if err != nil {
+		t.Errorf("ERROR: could not extract team map from test string: %s", err)
 	}
 	if !reflect.DeepEqual(expectedAccountsToTeams, actualAccountsToTeams) {
 		t.Errorf("ERROR: expected account to team map does not match actual. Expected: %#v, Actual: %#v", expectedAccountsToTeams, actualAccountsToTeams)
 	}
 
 	// this test checks that a duplicate account ID is caught
-	_, err = ParseTeamMap("team_map_test_duplicate.json")
-	fmt.Printf("err: %v\n", err)
+	duplicateStr, err := base64EncodeTestJSON("team_map_test_duplicate.json")
+	if err != nil {
+		t.Errorf("failed to read duplicate JSON file: %s", err)
+	}
+	_, err = ParseTeamMap(duplicateStr)
 	var duplicateAccountIDError *duplicateAccountIDError
 	if err == nil || !errors.As(err, &duplicateAccountIDError) {
 		t.Error("ERROR: didn't get expected error for duplicate account ID", err)
 	}
 
 	// Test invalid ARN
-	_, err = ParseTeamMap("team_map_test_invalid_arn.json")
-	fmt.Printf("err: %v\n", err)
+	invalidStr, err := base64EncodeTestJSON("team_map_test_invalid_arn.json")
+	if err != nil {
+		t.Errorf("failed to read invalid JSON file: %s", err)
+	}
+	_, err = ParseTeamMap(invalidStr)
 	var invalidRoleARNError *invalidRoleARNError
 	if err == nil || !errors.As(err, &invalidRoleARNError) {
 		t.Error("ERROR: didn't get expected error for invalid Role ARN", err)
